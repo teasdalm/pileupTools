c4d634895ecab5afe579dd5411319057486918fd
teasdalm <matthew@teasdale.xyz>
2016-04-05 16:01:45 +0100

    utility scripts

---
+++ bim2il.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash 
+
+# bim2table.sh
+# Script to convert bim to interval_list file
+
+# Filter file
+awk 'FS = "\t" {print "chr"$1":"$4"-"$4"}' $1
+
---
+++ bim2table.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash 
+
+# bim2table.sh
+# Script to convert bim to TABLE file 
+# Print header
+echo "HEADER	SNP-NAME	ALLELE-A	ALLELE-B"
+
+# Filter file
+awk 'FS = "\t" {print "chr"$1":"$4"-"$4"\t"$2"\t"$5"\t"$6}' $1 
+
