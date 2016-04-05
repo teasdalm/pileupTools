0db5e45f45cd72a3676b8d1d67c6a75ef6807afe
teasdalm <matthew@teasdale.xyz>
2016-04-05 11:28:09 +0100

    update

--- pileupTools.py
+++ pileupTools.py
@@ -1,24 +1,28 @@
+#! /usr/bin/python
+
 """
-The MIT License (MIT)
-
-Copyright (c) 2016 Matthew Teasdale
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+pileupTools
+A script to manipulate GATK bam files
+Matthew Teasdale 2016
+MIT licence
 """
 
+import argparse
+
+def parse_pileup_file(pileup_file):
+    pass
+
+def filter_bases:
+    pass
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Manipulate GATK pileup files')
+    parser.add_argument(dest='filenames', metavar='filename', nargs='*')
+    parser.add_argument('-q', metavar='Minimum base quality')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    my_args = get_arguments()
+    for each in my_args.filenames:
+        print each
